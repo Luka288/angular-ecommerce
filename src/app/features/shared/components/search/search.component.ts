371a574275fb0fc21654f97d3d7c4c91f3e6e955
Luka <lukagaxokidze28@gmail.com>
2024-12-27 20:04:39 +0400

    navigation corrected

--- src/app/features/shared/components/search/search.component.ts
+++ src/app/features/shared/components/search/search.component.ts
@@ -1,19 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
   searchControl = new FormControl('');
+  isVisible: boolean = false;
 
   ngAfterViewInit(): void {
     this.searchControl.valueChanges.pipe(debounceTime(300)).subscribe((res) => {
       console.log(res);
     });
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    event.preventDefault();
   }
 }
