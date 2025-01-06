1493133d88f847f9639cde200fa5fdf4c535b3fe
Luka <lukagaxokidze28@gmail.com>
2025-01-06 16:51:35 +0400

    product page updates, added bootstrap

--- src/app/features/product-page/product-page.component.ts
+++ src/app/features/product-page/product-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { SaveItemsService } from '../shared/services/save-items.service';
 import { ProductsService } from '../shared/services/products.service';
 import { single_item } from '../shared/interfaces/product.interface';
@@ -10,10 +10,11 @@ import {
 import { GalleriaModule } from 'primeng/galleria';
 import { responsiveOptions } from '../shared/consts/consts';
 import { CommonModule } from '@angular/common';
+import { SelectComponent } from '../shared/components/select/select.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [GalleriaModule, CommonModule, RouterModule],
+  imports: [GalleriaModule, CommonModule, RouterModule, SelectComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
@@ -21,11 +22,14 @@ export class ProductPageComponent {
   private readonly actSnap = inject(ActivatedRoute);
   private readonly perviousProducts = inject(SaveItemsService);
   private readonly everest = inject(ProductsService);
-  private readonly router = inject(ActivatedRoute);
+  private readonly router = inject(Router);
 
   singleItem: single_item | null = null;
   currentProductLib: thumbnailInterface[] = [];
   singleThumbnail: single_thumbnail[] = [];
+  arrayOfStock!: number;
+
+  selectedQuantity: number = 0;
 
   responsiveOptions = responsiveOptions;
 
@@ -37,12 +41,10 @@ export class ProductPageComponent {
       this.perviousProducts.saveItems(product_id!);
       this.loadSingle(product_id!);
     });
-
-    console.log('init');
   }
 
   loadSingle(_id: string) {
-    this.router.data.subscribe((res) => {
+    this.actSnap.data.subscribe((res) => {
       this.singleItem = res['singleItem_resolve'];
       console.log(res['singleItem_resolve']);
       this.singleItem?.images.forEach((image) => {
@@ -50,6 +52,11 @@ export class ProductPageComponent {
           thumbnailImageSrc: image,
         });
       });
+
+      // იღებს რესპონსიდან მხოლოდ სტოკს
+      this.arrayOfStock = res['singleItem_resolve'].stock;
+
+      console.log(this.arrayOfStock);
 
       this.singleThumbnail = [
         {
@@ -63,5 +70,19 @@ export class ProductPageComponent {
     const full = Math.floor(rating!);
     const half = rating! % 1 >= 0.5 ? 1 : 0;
     return { full, half };
+  }
+
+  // გადაყავს მომხმარებელი სერჩის ფეიჯზე სადაც არის
+  // მხოლო კონკრეტული ბრენიდს პროდუქცია
+  navigateToSearch(_brand: string) {
+    this.router.navigate(['/search'], {
+      queryParams: { query: _brand },
+    });
+    this.everest.searchProduct(_brand);
+  }
+
+  // იჭერს იუზერის არჩეულ რაოდენობას
+  catchQty(qty: number) {
+    this.selectedQuantity = qty;
   }
 }
---
+++ src/app/features/shared/components/select/select.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+
+@Component({
+  selector: 'app-select',
+  imports: [SelectModule, FormsModule, CommonModule, ReactiveFormsModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+})
+export class SelectComponent {
+  @Input({ alias: 'stock' }) stockOfProduct!: number;
+  @Output() quantitySelected = new EventEmitter<number>();
+
+  quantityControl = new FormControl(`1`, {
+    nonNullable: true,
+  });
+
+  ngOnChanges(SimpleChanges: SimpleChanges) {
+    console.log(this.stockOfProduct);
+  }
+
+  userQty() {
+    // გადასვემს პროდუქტის გვერდს მომხმარებლის არჩეულ
+    // რაოდენობას
+    if (this.quantityControl.valid) {
+      this.quantitySelected.emit(Number(this.quantityControl.value));
+    }
+  }
+}
