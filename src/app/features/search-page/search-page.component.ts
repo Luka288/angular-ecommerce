2cf1d0bbabaa66e004777acbdd868d42c0ae58f9
Luka <lukagaxokidze28@gmail.com>
2025-02-10 18:12:40 +0400

    add: infinite scroll to search page

--- src/app/features/search-page/search-page.component.ts
+++ src/app/features/search-page/search-page.component.ts
@@ -192,6 +192,9 @@ export class SearchPageComponent {
     }
   }
 
+  // ზრდის page - ის ლიმიტს 10 ით იმ შემთხვევაში
+  // როცა დირექტივა გადმოსცემს true იმ შემთხვეცაში როცა
+  // ახლანდელი scroll ის სიმაღლე და window სიმაღლე ტოლი იქნება
   loadMoreItems(event: boolean) {
     if (event === true) {
       this.page_size_infinite += 10;
--- src/app/features/shared/directives/infinite-scroll.directive.ts
+++ src/app/features/shared/directives/infinite-scroll.directive.ts
@@ -8,17 +8,27 @@ export class InfiniteScrollDirective {
 
   timeOut: boolean = false;
 
+  // ჰოსტ ლისტენერი სქროლის გასაკონტროლებლად აბრუმენს
+  // true იმ შემთხვევაში როცა window სიგანე არის 768px ან ნაკლები
+  // გათვლილია მხოლოდ ტელეფონის ეკრანებზე
   @HostListener('window:scroll', [])
   onScroll() {
     if (window.innerWidth > 768 || this.timeOut) {
       return;
     }
 
-    const scrollableHeight =
+    // ინახება დოკუმენტის სრული სქროლის სიმაღლე
+    const scrollHeight =
       document.documentElement.scrollHeight - window.innerHeight;
-    const currentScroll = window.scrollY;
 
-    if (currentScroll === scrollableHeight) {
+    // დოკუმენტის სქროლის პოზიცია
+    const currScroll = window.scrollY;
+
+    // თუ მაქსიმალური სქროლის სიმაღლე და მომხმარებლის
+    // სქროლის სიმაღლე გაუტოლდება ერთმანეთს ანუ თუ მომხმარებელი
+    // ბოლომდე ჩამოსქროლავს დირექტივა emit გაუკთებს true რაც უკვე
+    // მთავარ კომპონენტში იძახებს ფუნქციას სადაც პროდუქტები იტვირთება
+    if (currScroll === scrollHeight) {
       this.timeOut = true;
       this.bottom.emit(true);
       this.timeOut = false;
